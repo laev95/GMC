7f5117a4eb39d8853aade02e644712f723258279
leo <leo@manuskript>
2024-07-03 02:22:39 +0200

    Start parsing date.

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import serial
-
-PORT = "COM4"
-BAUDRATE = 115200
-CONN = serial.Serial(port=PORT, baudrate=BAUDRATE, stopbits=1)
-FLASH_SIZE = 0x00100000
+from config import CONN
 
 def get_hardware_model() -> str|None:
     CONN.write("<GETVER>>")
