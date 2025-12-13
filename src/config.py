37d2feadf12723c18b2424aa908104f14785c7f2
Leo <test@mail.de>
2025-12-13 01:45:11 +0100

    Change port to linux format.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import serial
 
-PORT = "COM3"
-BAUDRATE = 115200
-CONN = serial.Serial(port=PORT, baudrate=BAUDRATE, stopbits=1)
+PORT = "/dev/ttyUSB0"
+BAUD_RATE = 115200
+CONN = serial.Serial(port=PORT, baudrate=BAUD_RATE, stopbits=1)
 FLASH_SIZE = 0x00100000
