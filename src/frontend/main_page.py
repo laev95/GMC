8c60b86e7bb14b206b9eae07526a1209b077e7a8
Leo <test@mail.de>
2025-12-15 23:38:39 +0100

    Add basic web frontend.

---
+++ src/frontend/main_page.py
@@ -0,0 +1,10 @@
+from nicegui import ui
+from src.data.util import get_cpm
+
+@ui.refreshable
+def app():
+    is_turned_on, set_is_turned_on = ui.state(False)
+    ui.button("Stop reading CPM" if is_turned_on else "Start reading CPM", on_click=lambda: set_is_turned_on(not is_turned_on))
+    cpm_label = ui.label("CPM: ")
+    if is_turned_on:
+        ui.timer(1, lambda: cpm_label.set_text(f"CPM: {get_cpm()}"))
