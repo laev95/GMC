274a5146ae97e5a3864aab7e7a7ad3579645ba9d
Leo <laev1337@gmail.com>
2025-12-23 18:44:26 +0100

    Refactor `main.py` to encapsulate app execution in a `main` function.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from frontend.main_page import app, ui
-from startup.startup import connect
 
-try:
-    _ = connect()
-except OSError as exc:
-    print(f"Failed to connect to device: {exc}")
-    quit(1)
 
-app()
-ui.run()
+def main():
+    app()
+    ui.run()
+
+
+if __name__ == "__main__":
+    main()
